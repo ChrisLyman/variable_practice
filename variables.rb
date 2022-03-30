7ca869b85bdba3e8f5d4158e328224f5af6bee60
Chris Lyman <christopher.lyman0@gmail.com>
2022-03-30 08:27:45 -0600

    Add comments

--- variables.rb
+++ variables.rb
@@ -1,5 +1,8 @@
+#Delcares the variable home and assigns the string Arvada to it
 home = "Arvada"
+#Declares the varaiable phone_number and assigns the string 123-456-7890 to it
 phone_number = "123-456-7890"
+#Declares the variable
 bank_account = 123456
 number_of_people = 2
 bank_balance = 1234.67
